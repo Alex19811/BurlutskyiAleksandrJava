608f4f874048612672bb05f6a9e7167476a98a09
Aleksandr <aleksandrburlutskiy@gmail.com>
2019-06-06 15:17:22 +0300

    06062019

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,6 +9,8 @@ public class Main {
         System.out.println("          *        *   *        *   *       *   *");
         System.out.println("     *    *        *****         * *        *****");
         System.out.println("       ***         *   *          *         *   *");
+        System.out.println("       (A)          (B)          (C)         (B)");
+
 
     }
 }

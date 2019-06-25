d3e758a0cf408d8654dcd597b9d02722fc04b37c
Aleksandr <aleksandrburlutskiy@gmail.com>
2019-06-25 16:21:35 +0300

    25062019

---
+++ src/com/company/LabWork1_4.java
@@ -0,0 +1,31 @@
+package com.company;
+
+class main {
+    public static void main(String[] args) {
+        byte a = 5;
+        int b = 1;
+        short c = 25;
+        long d = 525641;
+        float e = 1.0f;
+        double f = 1.35;
+        char g=88;
+        boolean h=false;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+        System.out.println(h);
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(c/a);
+        System.out.println(c*b);
+
+
+
+
+    }
+}

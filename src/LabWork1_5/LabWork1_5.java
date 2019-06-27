682f05749f10bdb4328b00ec8f786c98d5e21c7c
Aleksandr <aleksandrburlutskiy@gmail.com>
2019-06-27 17:22:21 +0300

    27062019

--- src/LabWork1_5/LabWork1_5.java
+++ src/LabWork1_5/LabWork1_5.java
@@ -5,9 +5,10 @@ public class LabWork1_5 {
     public static void main(String[] args) {
         for (int i = 1; i < 9; i++) {
 
-            for (int j = 1; j <= 8; j--) {
-              //  System.out.println(j);
+            for (int j = i; j <= 0; j--) {
+                //System.out.println(j);
             }
+
             System.out.println(i);
 
         }

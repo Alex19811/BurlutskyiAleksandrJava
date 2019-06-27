8fcddf33342fa5ba31418fadc12b28accfada387
Alex19811 <49365019+Alex19811@users.noreply.github.com>
2019-06-27 21:30:50 +0300

    27062019

--- src/LabWork1_5/LabWork1_5.java
+++ src/LabWork1_5/LabWork1_5.java
@@ -3,14 +3,15 @@ package LabWork1_5;
 public class LabWork1_5 {
 
     public static void main(String[] args) {
+
         for (int i = 1; i < 9; i++) {
 
-            for (int j = i; j <1; j--) {
+            for (int j = i; j >= 1; j--) {
               System.out.print(j);
-                System.out.println(j);
+
             }
 
-
+            System.out.println();
 
         }
     }
--- src/LabWork2_2_1/Matrix.java
+++ src/LabWork2_2_1/Matrix.java
@@ -1,46 +1,46 @@
-package LabWork2_2_1;
-
-public class Matrix {
-
-    //public int [][] multipliMatrices (int [][] a, int [][]b){
-    // result
-    // }
-
-    public int [][] addition int [][] matrix) {
-        this(inMatrixSuitable()).matrix=matrix;
-    }
-
-
-    public Matrix addition  (int [][] a, int [][]b) {
-        if(inMatrixSuitable(a,b)){
-            int [][] result= new int[a.length] [a[0].length]
-            for (int i=0; i<a.length; i++);{
-                for (int J=0; J<a[i].length;j++){
-
-                }
-            }
-
-
-        }
-            return true;
-
-    }
-
-
-
-    private boolean inMatrixSuitable(int [][] a, int [][] b){
-    for (int i=0; i<a.length; i++) {
-        if (a[i].length !=b [i].length);
-    }
-
-
-    }
-
-    private Matrix convertArrayInMatrix{
-
-    }
-
-    public void printMatrix(int[][] addition) {
-        this.addition = addition;
-    }
-}
+//package LabWork2_2_1;
+//
+//public class Matrix {
+//
+//    //-public int [][] multipliMatrices (int [][] a, int [][]b){
+//    // -result
+//    // -}
+//
+//    public int [][] addition int [][] matrix) {
+//        this(inMatrixSuitable()).matrix=matrix;
+//    }
+//
+//
+//    public Matrix addition  (int [][] a, int [][]b) {
+//        if(inMatrixSuitable(a,b)){
+//            int [][] result= new int[a.length] [a[0].length]
+//            for (int i=0; i<a.length; i++);{
+//                for (int J=0; J<a[i].length;j++){
+//
+//                }
+//            }
+//
+//
+//        }
+//            return true;
+//
+//    }
+//
+//
+//
+//    private boolean inMatrixSuitable(int [][] a, int [][] b){
+//    for (int i=0; i<a.length; i++) {
+//        if (a[i].length !=b [i].length);
+//    }
+//
+//
+//    }
+//
+//    private Matrix convertArrayInMatrix{
+//
+//    }
+//
+//    public void printMatrix(int[][] addition) {
+//        this.addition = addition;
+//    }
+//}

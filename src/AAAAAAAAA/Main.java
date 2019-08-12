8b81314bc8ffb9dd79d4ffa8fc8295d766326e46
Aleksandr <aleksandrburlutskiy@gmail.com>
2019-08-12 17:39:32 +0300

    11082019

---
+++ src/AAAAAAAAA/Main.java
@@ -0,0 +1,84 @@
+package AAAAAAAAA;
+
+public class Main {
+    public static void main(String[] args) {
+//String letters = "ABCAB";
+//
+//        System.out.println(letters.startsWith("Ab"));
+
+        //8????????
+        //String result = "Sunday".replace(' ', 'Z').trim().concat("M n");
+//        System.out.println(result);
+
+      //  System.out.println("Morning"==morning2);
+        //String letters= "ABCAB";
+//        System.out.println(letters.substring(0,2).startsWith('A'));
+
+
+
+        //String M1 = "Mo";
+//        System.out.println("Mo"== M1);
+
+
+
+
+
+
+//StringBuilder sb2 = new StringBuilder("123");
+//char[]name = new char[]{'p','a','u','l'};
+//String boy = new String(name);
+//StringBuilder sd1 =new StringBuilder("StringBuilder");
+//String str5 = new String(sd1);
+//        StringBuilder sd2 =new StringBuilder("StringBuilder");
+//        String str6 = new String(sd2);
+//        String empName=null;
+//        //System.out.println(sb2);
+
+
+
+
+
+
+//        Double d1 = Double.parseDouble("3.14");
+//        Double d2 = Double.parseDouble("3.14");
+//        System.out.println("d1="+d1+";d2="+d2);
+
+//        Float f1 = new Float(3.1f);
+//        Float f2 = new Float("3.1f");
+//        Float f3 = Float.valueOf("3.1f");
+//        System.out.println(f1+""+f2+f3);
+
+//        Main u = new Main ();
+//        u.go(5);
+//    }
+//        boolean go (Integer i){
+//            Boolean ifSo=true;
+//            Short s = 300;
+//            if (ifSo){
+//                System.out.println(++s);
+//
+//            }
+//            return ifSo;
+
+//        10
+//        int i1 = int.parseInt("1");
+//        int i2 = int.parseInt("4");
+//
+//        System.out.println(i1+i2);
+
+
+//Integer i3 = 100;
+//Integer i4 = 100;
+//if (i3==i4) System.out.println("same");
+//if (i3.equals(i4)) System.out.println("mean");
+
+//        System.out.println(Integer.parseInt("123"));
+//        System.out.println(Integer.parseInt("123_45"));
+//        System.out.println(Integer.parseInt("12ABCD"));
+//        System.out.println(Integer.parseInt("+123"));
+//        System.out.println(Integer.parseInt("-123"));
+    }
+
+
+}
+

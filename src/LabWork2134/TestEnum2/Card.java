697c821abd71b77a479c3c3f40b45c2f759aa263
Aleksandr <aleksandrburlutskiy@gmail.com>
2019-08-22 16:06:31 +0300

    22082019

--- src/LabWork2134/TestEnum2/Card.java
+++ src/LabWork2134/TestEnum2/Card.java
@@ -1,21 +1,36 @@
 package LabWork2134.TestEnum2;
 
-/*
 public class Card {
-    private Suit cardSuitof;
-    private Rank cardRank;
+    private Main.Suit cardSuit;
+    private Main.Rank cardRank;
 
-    public Card(Suit cardSuitof, Rank cardRank) {
-        this.cardSuitof = cardSuitof;
+    public Card(Main.Suit cardSuit, Main.Rank cardRank) {
+        this.cardSuit = cardSuit;
         this.cardRank = cardRank;
     }
 
     @Override
     public String toString() {
         return "Card{" +
-                "cardSuitof=" + cardSuitof +
+                "cardSuit=" + cardSuit +
                 ", cardRank=" + cardRank +
                 '}';
     }
+
+}
+
+/*
+public class Card {
+    int[] deck = new int[52];
+    String[] suits = {"Spades", "Hearts", "Diamonds", "Clubs"};
+    String[] ranks = {"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"};
+
+    Card() {
+        for (int i = 0; i < deck.length; i++) {
+            deck[i] = i;
+        }
+    }
+
+
 }
 */
--- src/LabWork2134/TestEnum2/Main.java
+++ src/LabWork2134/TestEnum2/Main.java
@@ -1,40 +1,120 @@
-/*
 
 package LabWork2134.TestEnum2;
 
+import com.sun.prism.impl.ps.CachingShapeRep;
+
+import javax.smartcardio.Card;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Main {
+
+    public enum Suit {SPADE, DIAMOND, CLUB, HEART}
+
+    public enum Rank {ACE, TWO, THREE, FOUR, FIX, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING}
+
     public static void main(String[] args) {
 
-        for (Suit i : Suit.values()) {
-            for (Rank j : Rank.values()) {
 
-                //System.out.println(i + j);
+        List<Card> newDeck = new ArrayList<Card>( );
 
-                String[][] card = new String[4][13];
+        Rank[] ranks = Rank.values();
+        Suit[] suits = Suit.values();
 
-//                array[0][0] = 1;
-//                array[0][1] = 2;
-//                array[1][0] = 3;
-//                array[1][1] = 4;
-
-                for (int i = 0; i < card.length; i++) {
-                    for (int j = 0; j < card[i].length; j++) {
-                        System.out.print(card[i][j] + "\t");
-                    }
-                    System.out.println();
-
-                }
-
+        for (Suit suit : suits) {
+            for (Rank rank : ranks) {
+                boolean add = newDeck.add(new Card(rank, suit));
             }
+        }
 
+        int n = newDeck.size();
 
+        for (int i = 0; i < n ; i++) {
+            System.out.println((i + 1)  + ": " + newDeck.get(i));
         }
     }
-    public enum Suit {SPADE, DIAMOND, CLUB, HEART}
-    public enum Rank {ACE, TWO, THREE, FOUR, FIX, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING}
 }
 
 
 
+       /* *//*
+        for (Suit s : Suit.values()) {
+            for (Rank r : Rank.values()) {
+                Card c = new Card(s, r);
+            }
+            System.out.println(c);
+        }
+    }
 
-*/
+    private List<Card> cards = new ArrayList<>();
+
+    public void shuffle() {
+        Collections.shuffle(Collections.singletonList(this.cards));
+    }
+    */
+
+
+
+
+
+      /*  int[] deck = new int[52];
+        String[] suits = {"Spades", "Hearts", "Diamonds", "Clubs"};
+        String[] ranks = {"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"};
+
+        // Initialize cards
+        for (int i = 0; i < deck.length; i++) {
+            deck[i] = i;
+        }
+
+        // Shuffle the cards
+        for (int i = 0; i < deck.length; i++) {
+            int index = (int) (Math.random() * deck.length);
+            int temp = deck[i];
+            deck[i] = deck[index];
+            deck[index] = temp;
+        }
+
+        // Display the all the cards
+        for (int i = 0; i < 52; i++) {
+            String suit = suits[deck[i] / 13];
+            String rank = ranks[deck[i] % 13];
+            System.out.println(rank + " of " + suit);
+        }
+    }*/
+
+//        array card = new Card() {
+//        }
+//        for (Suit a : Suit.values()) {
+//            for (Rank b : Rank.values()) {
+//
+//                //System.out.println(i + j);
+//
+//                String[][] card = new String[4][13];
+//
+//                array[0][0] = 1;
+//                array[0][1] = 2;
+//                array[1][0] = 3;
+//                array[1][1] = 4;
+//
+//                for (int i = 0; i < card.length; i++) {
+//                    for (int j = 0; j < card[i].length; j++) {
+//                        // System.out.print(card[i][j] + "\t");
+//                    }
+//                    System.out.println();
+//
+//                }
+//
+//            }
+//
+//
+//        }
+//    }
+//}
+//
+//
+//
+//
+//
+//
+//

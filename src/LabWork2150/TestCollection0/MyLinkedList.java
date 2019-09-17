3553d99805c760d09aae8d45ef5ba06b2da3384d
Alex19811 <49365019+Alex19811@users.noreply.github.com>
2019-09-17 21:30:34 +0300

    17092019

---
+++ src/LabWork2150/TestCollection0/MyLinkedList.java
@@ -0,0 +1,180 @@
+package LabWork2150.TestCollection0;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
+public class MyLinkedList<E, _> {
+    Node head;
+
+    public boolean add(E value) {
+        if (head == null) {
+            head = new Node(value);
+        }
+        else {
+            Node current = head;
+            while (current.next != null){
+                current=current.next;
+            }
+                current.next = new Node(value);
+        }
+
+        return false;
+    }
+
+    public void printAllValue() {
+        Node current = head;
+        while (current != null) {
+            System.out.println(current.value);
+            current=current.next;
+
+        }
+    }
+    public  int removeElement(E elementToRemove ) {
+        Node current = head;
+        Node prev = null;
+        int counter = 0;
+        while (current != null) {
+
+            if (current.value.equals(elementToRemove)) {
+                if (prev == null) {
+                    head = current.next;
+                }
+                else {prev.next = current.next;
+
+
+                }
+                counter++;
+            }
+
+            prev = current;
+            current.next = current.next;
+        }
+        return counter;
+    }
+
+}
+
+
+
+
+
+/*
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return false;
+    }
+
+    @Override
+    public Iterator iterator() {
+        return null;
+    }
+
+    @Override
+    public Object[] toArray() {
+        return new Object[0];
+    }
+
+    @Override
+    public boolean add(E value) {
+        if
+        return false;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(Collection c) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(int index, Collection c) {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public Object get(int index) {
+        return null;
+    }
+
+    @Override
+    public Object set(int index, Object element) {
+        return null;
+    }
+
+    @Override
+    public void add(int index, Object element) {
+
+    }
+
+    @Override
+    public Object remove(int index) {
+        return null;
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public ListIterator listIterator() {
+        return null;
+    }
+
+    @Override
+    public ListIterator listIterator(int index) {
+        return null;
+    }
+
+    @Override
+    public List subList(int fromIndex, int toIndex) {
+        return null;
+    }
+
+    @Override
+    public boolean retainAll(Collection c) {
+        return false;
+    }
+
+    @Override
+    public boolean removeAll(Collection c) {
+        return false;
+    }
+
+    @Override
+    public boolean containsAll(Collection c) {
+        return false;
+    }
+
+    @Override
+    public Object[] toArray(Object[] a) {
+        return new Object[0];
+    }
+*/
+
---
+++ src/LabWork2150/TestCollection0/Node.java
@@ -0,0 +1,11 @@
+package LabWork2150.TestCollection0;
+
+public class Node <T>{
+
+    T value;
+    Node  next;
+
+    public Node (T value) {
+        this.value=value ;
+    }
+}

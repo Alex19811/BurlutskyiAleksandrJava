83aa982819ff139a38b6500397b2c467e65fea99
Aleksandr <aleksandrburlutskiy@gmail.com>
2019-10-06 21:26:12 +0300

    06102019

---
+++ src/LabWork2173/TestTread2/Main.java
@@ -0,0 +1,46 @@
+package LabWork2173.TestTread2;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        MySumCount mySumCount = new MySumCount();
+        MySumCount mySumCount1 = new MySumCount();
+
+
+        mySumCount.setStartindex(0);
+        mySumCount.setStopindex(100);
+        mySumCount1.setStartindex(0);
+        mySumCount1.setStopindex(100);
+
+        mySumCount.myArrayAdd();
+        mySumCount1.myArrayAdd();
+
+        Thread thread = new Thread(mySumCount);
+        Thread thread1 = new Thread(mySumCount1);
+
+        thread.start();
+        thread1.start();
+    }
+}
+
+
+//        for (int i =0; i <= maxNumb; i++) {
+//            maxNumb = (int) (Math.random() * 64);
+//            System.out.println(i);
+//
+//
+//mySumCount.start();
+//mySumCount1.start();
+//
+//        int myArray[ 1000];
+//        MySumCount myArray = MySumCount(1, 10);
+//        ArrayList<Integer> myArray = new ArrayList<>();
+//        for (int i = 0; i <= 1000; i++) {
+////        list1.add("num" + i);
+//
+//        }
+//    }
+//}
---
+++ src/LabWork2173/TestTread2/MySumCount.java
@@ -0,0 +1,89 @@
+package LabWork2173.TestTread2;
+
+import java.util.ArrayList;
+
+
+public class MySumCount extends Thread {
+    int startindex;
+    int stopindex;
+    long resultSum;
+
+
+    public MySumCount(Runnable target, int startindex, int stopindex) {
+        super(target);
+        this.startindex = startindex;
+        this.stopindex = stopindex;
+    }
+
+    public int getStartindex() {
+        return startindex;
+    }
+
+    public int getStopindex() {
+        return stopindex;
+    }
+
+    public void setStartindex(int startindex) {
+        this.startindex = startindex;
+    }
+
+    public void setStopindex(int stopindex) {
+        this.stopindex = stopindex;
+    }
+
+    ArrayList<Integer> myArray = new ArrayList<>();
+
+    public void myArrayAdd() {
+        for (int i = getStartindex(); i <= getStopindex(); i++) {
+            myArray.add((int) (Math.random() * 1000));
+            System.out.println(i);
+            System.out.println(myArray);
+        }
+    }
+
+    public void setMyArray(ArrayList<Integer> myArray) {
+        this.myArray = myArray;
+    }
+
+    public long getResultSum() {
+        return resultSum;
+    }
+
+    public MySumCount() {
+
+        super();
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i >= 1000; i++) {
+            for (int num : myArray)
+                resultSum = resultSum + num;
+            System.out.println(resultSum);
+
+
+
+////
+//            int sum = 0;
+//            // цикл для обхода каждого элемента массива
+//            for (int num : list1) {
+//
+//                // суммирование каждого элемента массива
+//                sum = sum + num;
+//            }
+//            System.out.println("Сумма элементов массива равна: " + sum);
+        }
+
+
+    }
+}
+//        for (int i = 10; i >= 0; i--) {
+//            System.out.println(i);
+//            //super.run();
+//            try{
+//                sleep(1000);
+//            } catch (InterruptedException e){
+//                e.printStackTrace();
+//            }
+//}
+

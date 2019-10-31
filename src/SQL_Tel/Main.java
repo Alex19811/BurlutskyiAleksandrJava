5515f7448362acf5692e60a65d56a0baa54c4d06
Alex19811 <49365019+Alex19811@users.noreply.github.com>
2019-10-31 21:38:27 +0200

    31102019

---
+++ src/SQL_Tel/Main.java
@@ -0,0 +1,19 @@
+package SQL_Tel;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+
+import static java.lang.reflect.Modifier.PRIVATE;
+
+public class Main {
+    public static void main(String[] args) {
+        Class userClass = Class.class;
+        Field[] fields = userClass.getFields();
+        System.out.println(fields.length);
+        for(Field field :fields){
+
+        }
+        Modifier.PRIVATE;
+        System.out.println(userClass.getSuperclass());
+    }
+}
---
+++ src/SQL_Tel/User.java
@@ -0,0 +1,8 @@
+package SQL_Tel;
+
+public class User extends Humen {
+    private int age;
+    public String lastname;
+    protected String falsname;
+
+}

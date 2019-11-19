ae48c9ec6094da21f2c3570abf8f1019d46a4f44
Aleksandr <aleksandrburlutskiy@gmail.com>
2019-11-19 16:46:09 +0200

    19112019

--- src/LabWork22161_TestAnnotation1/VarArgs.java
+++ src/LabWork22161_TestAnnotation1/VarArgs.java
@@ -1,30 +1,49 @@
 package LabWork22161_TestAnnotation1;
 
+import javafx.scene.input.KeyCode;
+
 import java.util.Arrays;
+
+import static javafx.scene.input.KeyCode.T;
 
 public class VarArgs {
 
-    @SafeVarargs
-    static void findMax(int... array) {
+    @Deprecated
+    static void findMax(int... varargs) {
 
-            System.out.println("Количество аргументов: " + array.length);
+            System.out.println("Количество аргументов: " + varargs.length);
 
-            for (int a : array) {
+            for (int a : varargs) {
                 System.out.print(a + " ");
 
-                int min = Arrays.stream(array).min().getAsInt();
-                int max = Arrays.stream(array).max().getAsInt();
+                int min = Arrays.stream(varargs).min().getAsInt();
+                int max = Arrays.stream(varargs).max().getAsInt();
               System.out.println("Min = " + min);
                 System.out.println("Max = " + max);
             }
            }
 
-
-        public static void main(String[] args) {
+    @SuppressWarnings("deprecation")
+        public static <T> void main(String[] args) {
             findMax();
             findMax(1);
             findMax(1, 2);
             findMax(new int[]{1, 3});
+
+      /*  T[] args;
+        static  <T> T[] findMax1(T[] args){
+            System.out.println("Количество аргументов: " + args.length);
+            for (T a : args) {
+                System.out.print(a + " ");
+            }
+            System.out.println();
+        }*/
+
         }
+
+    private static void findMax1(KeyCode t) {
     }
+
+}
+
 
